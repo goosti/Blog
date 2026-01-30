c9f59cf018dab82e0f491bbe66b2215ba7e3aa0f
goosti <remisd@orange.fr>
2026-01-30 13:33:22 +0100

    Inject ArticleRepository into MainController index method and retrieve all articles

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+
+        $articles = $articleRepository->findAll();
+
+        dd($articles);
 
         $articles = [
         [
