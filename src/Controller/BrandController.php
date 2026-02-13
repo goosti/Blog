8c2610e85a79305994c13d2d0ef3379beb8ff67c
goosti <remisd@orange.fr>
2026-02-13 15:20:29 +0100

    feat: add Brand creation form and associated template; implement form handling in BrandController

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Brand;
+use App\Form\BrandType;
 use App\Repository\BrandRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,6 +24,26 @@ final class BrandController extends AbstractController
         ]);
     }
 
+    #[Route('/brand/new', name: 'app_brand')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $brand = new Brand();
+        $form = $this->createForm(BrandType::class, $brand);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($brand);
+            $em->flush();
+
+            return $this->redirectToRoute('app_brand');
+        }
+
+        return $this->render('brand/new.html.twig', [
+            'form' => $form,
+        ]);
+}
     #[Route('/brand/{id}', name: 'app_brand_show')]
     public function show(int $id, BrandRepository $brandRepository): Response
     {
